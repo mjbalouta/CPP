23b711ffc667e5ec35d2edd3bde59009da9c57a0
Maria João Balouta <165146704+mjbalouta@users.noreply.github.com>
2025-11-28 18:48:24 +0000

    Fixed typo in feedback message

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -16,7 +16,7 @@
 int main(int ac, char **av)
 {
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOIDE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		for (int i = 1; i < ac; i++)
