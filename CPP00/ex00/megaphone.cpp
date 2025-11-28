54a5cc5a4b1da5f8b09922973cf2147bb1097d7c
mjbalouta <mjbalouta@gmail.com>
2025-11-28 15:31:52 +0000

    ex00 added

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjoao-fr <mjoao-fr@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 14:15:53 by mjoao-fr          #+#    #+#             */
+/*   Updated: 2025/11/28 15:04:01 by mjoao-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOIDE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int z = 0; av[i][z]; z++)
+			{
+				char upper = std::toupper(av[i][z]);
+				std::cout << upper;
+			}
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
