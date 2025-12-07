212a559fe0896f622e757e12b183a818bbf35de9
mjoao-fr <mjbalouta@gmail.com>
2025-12-07 15:36:15 +0000

    fixed syntax

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mjoao-fr <mjoao-fr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 14:15:53 by mjoao-fr          #+#    #+#             */
-/*   Updated: 2025/11/28 15:04:01 by mjoao-fr         ###   ########.fr       */
+/*   Updated: 2025/12/07 15:34:27 by mjoao-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,7 @@ int main(int ac, char **av)
 		for (int i = 1; i < ac; i++)
 		{
 			for (int z = 0; av[i][z]; z++)
-			{
-				char upper = std::toupper(av[i][z]);
-				std::cout << upper;
-			}
+				std::cout << static_cast<char>(std::toupper(av[i][z]));
 		}
 		std::cout << std::endl;
 	}
