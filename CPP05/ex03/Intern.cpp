3bbd771e863d54bc92840e11780335a5524a11a4
Maria Balouta <mjbalouta@gmail.com>
2026-01-30 15:37:47 +0000

    fix: removed if/else

--- CPP05/ex03/Intern.cpp
+++ CPP05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: mjoao-fr <mjoao-fr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 13:10:39 by mjoao-fr          #+#    #+#             */
-/*   Updated: 2026/01/30 13:29:49 by mjoao-fr         ###   ########.fr       */
+/*   Updated: 2026/01/30 15:36:47 by mjoao-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,25 @@ Intern::~Intern() {}
 
 AForm* Intern::makeForm(const std::string& name, const std::string& target)
 {
-	if (name == "shrubbery form")
-		return new ShrubberyCreationForm(target);
-	if (name == "robotomy form")
-		return new RobotomyRequestForm(target);
-	if (name == "presidential form")
-		return new PresidentialPardonForm(target);
-	throw AForm::FormNotFoundException();
+	std::string list[3] = {"shrubbery form", "robotomy form", "presidential form"};
+	int i = 0;
+	int id = -1;
+
+	for (i = 0; i < 3; i++)
+	{
+		if (name == list[i])
+			id = i;
+	}
+
+	switch (id)
+	{
+		case 0:
+			return new ShrubberyCreationForm(target);
+		case 1:
+			return new RobotomyRequestForm(target);
+		case 2:
+			return new PresidentialPardonForm(target);
+		default:
+			throw AForm::FormNotFoundException();
+	}
 }
