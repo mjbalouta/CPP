f376b019bec107da418bedfb961359341aae456c
mjbalouta <mjbalouta@gmail.com>
2026-02-09 17:10:36 +0000

    fix: null pointer handling in cpp06 ex02

--- CPP06/ex02/Base.cpp
+++ CPP06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: mjoao-fr <mjoao-fr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 11:33:27 by mjoao-fr          #+#    #+#             */
-/*   Updated: 2026/02/06 13:09:24 by mjoao-fr         ###   ########.fr       */
+/*   Updated: 2026/02/09 17:08:18 by mjoao-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,24 @@ Base* generate(void)
 {
 	int nr = rand() % 3;
 
-	if (nr == 0)
-		return new A;
-	else if (nr == 1)
-		return new B;
-	else
-		return new C;
+	switch (nr)
+	{
+		case 0:
+			return new A;
+		case 1:
+			return new B;
+		case 2:
+			return new C;
+		default:
+			return NULL;
+	}
+	return NULL;
 }
 
 void identify(Base* p)
 {
+	if (!p)
+		std::cout << "Null pointer." << std::endl;
 	if (dynamic_cast<A*>(p))
 		std::cout << "This is an A." << std::endl;
 	else if (dynamic_cast<B*>(p))
@@ -44,21 +52,21 @@ void identify(Base& p)
 {
 	try
 	{
-		dynamic_cast<A&>(p);
+		(void)dynamic_cast<A&>(p); //(void) because i don't want to use the result, I just want to know if the cast throws an exception
 		std::cout << "This is an A." << std::endl;
 		return;
 	}
 	catch(std::exception&) {}
 	try
 	{
-		dynamic_cast<B&>(p);
+		(void)dynamic_cast<B&>(p);
 		std::cout << "This is a B." << std::endl;
 		return;
 	}
 	catch(std::exception&) {}
 	try
 	{
-		dynamic_cast<C&>(p);
+		(void)dynamic_cast<C&>(p);
 		std::cout << "This is a C." << std::endl;
 		return;
 	}
--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mjoao-fr <mjoao-fr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 13:03:53 by mjoao-fr          #+#    #+#             */
-/*   Updated: 2026/02/06 13:10:46 by mjoao-fr         ###   ########.fr       */
+/*   Updated: 2026/02/09 17:09:50 by mjoao-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ int main(void)
 	
 	Base* b = generate();
 	Base& a = *b;
+	Base* nullPointer = NULL;
 	identify(a);
 	identify(b);
+	identify(nullPointer);
 
 	delete b;
 	return 0;
