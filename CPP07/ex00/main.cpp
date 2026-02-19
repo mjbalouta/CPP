64cd107612a4556b1332aff98ce0cac54cabf1bc
mjbalouta <mjbalouta@gmail.com>
2026-02-19 14:31:40 +0000

    fix: added a more detailed main for testing

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mjoao-fr <mjoao-fr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 16:23:44 by mjoao-fr          #+#    #+#             */
-/*   Updated: 2026/02/17 10:14:55 by mjoao-fr         ###   ########.fr       */
+/*   Updated: 2026/02/19 11:28:40 by mjoao-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,21 @@ int main()
 {
 	int a = 2;
 	int b = 3;
+	std::cout << "initial values of a and b: " << std::endl
+	<< "a = " << a << ", b = " << b << std::endl;
 	::swap(a, b);
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "values after swap: " << std::endl << "a = " << a 
+	<< ", b = " << b << std::endl;
 	std::cout << "min(a,b) = " << ::min(a,b) << std::endl;
 	std::cout << "max(a,b) = " << ::max(a,b) << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	std::cout << "initial values of c and d: " << std::endl
+	<< "c = " << c << ", d = " << d << std::endl;
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "values after swap: " << std::endl << "c = " << c 
+	<< ", d = " << d << std::endl;
 	std::cout << "min(c,d) = " << ::min(c,d) << std::endl;
 	std::cout << "max(c,d) = " << ::max(c,d) << std::endl;
 	
